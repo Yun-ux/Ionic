de7b4931e0e5fa180d23164b14deedb0e7a3a699
Yun <hinamoriyasmina@gmail.com>
2019-12-18 13:38:14 +0100

    18/12/19 AM commit

--- src/app/exercice2/exercice2.module.ts
+++ src/app/exercice2/exercice2.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { Exercice2PageRoutingModule } from './exercice2-routing.module';
 
 import { Exercice2Page } from './exercice2.page';
+import {IonicStorageModule} from '@ionic/storage';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    Exercice2PageRoutingModule
+    Exercice2PageRoutingModule,
+    IonicStorageModule.forRoot()
   ],
   declarations: [Exercice2Page]
 })
--- src/app/exercice2/exercice2.page.ts
+++ src/app/exercice2/exercice2.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from "../_models/article";
+import { AlertController } from '@ionic/angular';
+import {Storage} from '@ionic/storage';
+
+
+
 
 @Component({
   selector: 'app-exercice2',
@@ -7,15 +13,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice2Page implements OnInit {
   variable = null;
-  list = [];
+  list : Article[];
+  
 
-  constructor() { }
+  constructor(
+    private alertController: AlertController,
+    private storage: Storage  
+  ) { 
+ 
+  }
 
   ngOnInit() {
+    this.storage.get("Articles").then(data => {
+      this.list = data || [];
+    });
+   
+    
   }
 
   add() {
-    this.list.push(this.variable);
+    this.list.push({name: this.variable, isChecked: false});
     this.variable = null;
+    this.save();
+  }
+  cocher(item: Article) {
+    item.isChecked = !item.isChecked;
+    this.save();
+  }
+  delete(item: Article) {
+    let index = this.list.indexOf(item);
+    this.list.splice(index,1);
+  }
+  showAlert(item: Article) {
+    this.alertController.create({
+      header: "Attention",
+      subHeader: "Etes-vous sur de vouloir supprimer l'article " + item.name,
+      buttons: [{text: 'Oui', handler: () => this.delete(item)}, {text: 'Non'}]
+    })
+    .then(alert => alert.present());
+  }
+
+  private save() {
+    this.storage.set('Articles', this.list);
   }
 }
